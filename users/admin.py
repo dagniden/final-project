9001030df425b4e3034d8267f86d9ae3800f06e5
Daniil Gnidenko <dagnidenko@gmail.com>
2026-03-29 17:57:23 +0300

    Регистрация моделей в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("email",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "phone_number",
+                    "avatar",
+                    "city",
+                    "telegram_chat_id",
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "classes": ("wide",),
+                "fields": ("phone_number", "avatar", "city", "telegram_chat_id"),
+            },
+        ),
+    )
